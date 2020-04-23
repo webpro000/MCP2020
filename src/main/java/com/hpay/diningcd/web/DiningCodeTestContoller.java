b60721e05bf1c6c419cbf9be2933ed4f3b943250
loudness84 <goodnickname84@gmail.com>
2020-04-23 15:57:32 +0900

    진행중

--- src/main/java/com/hpay/diningcd/web/DiningCodeTestContoller.java
+++ src/main/java/com/hpay/diningcd/web/DiningCodeTestContoller.java
@@ -7,7 +7,10 @@ import able.com.service.prop.PropertyService;
 import able.com.web.HController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +18,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.hpay.diningcd.service.DincdService;
@@ -27,6 +32,7 @@ import com.hpay.diningcd.vo.DincdVO2;
 import com.hpay.diningcd.vo.Item;
 import com.hpay.diningcd.vo.Item_additional;
 import com.hpay.diningcd.vo.Item_basic;
+import com.hpay.hsptl.vo.HsptVO;
 import com.hpay.common.vo.HpayLogVO;
 import com.hpay.notice.vo.NoticeVO;
 
@@ -43,6 +49,8 @@ import java.util.Locale;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.ArrayList;
@@ -106,9 +114,11 @@ public class DiningCodeTestContoller extends HController {
            
             item3.setLongitude(dincdVO.getLongitude());
             
+                        
             dincdVO2.getBasic_data().add(item3);
-                                    
-               
+            
+            Item_additional Item4 = new Item_additional();
+           
              
         }catch(Exception e){
             e.printStackTrace();
@@ -120,39 +130,46 @@ public class DiningCodeTestContoller extends HController {
     }
     
     @RequestMapping(value="/DincdJsonPars.do")
-    public String ParmarcyListCall() throws java.text.ParseException{
+    public String ParmarcyListCall() throws java.text.ParseException, JsonParseException, JsonMappingException, IOException{
         
         //Unmarshaller unmarshaller;
-        String Url = "http://localhost:8080/DincdTest.do";
+        String Url = "http://localhost:8080/Diningcd_top100_3.json";
+        
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        
+        MultiValueMap<String, String> parm = null;
+        
+        factory.setReadTimeout(10000);
+        factory.setConnectTimeout(10000);
         
         RestTemplate restTemplate = new RestTemplate();
-        String ReturnJson = restTemplate.postForObject(Url, "", String.class);       
-        JSONParser parser = new JSONParser();
-        JSONObject obj = null;        
-        try{
-            
-            obj = (JSONObject)parser.parse(ReturnJson);
-            
-            
-        }catch(ParseException e){
-            System.out.println("변환에 실패");
-            e.printStackTrace();
+        String ReturnJson = restTemplate.postForObject(Url, "", String.class);  // XML to Json
+        System.out.println(ReturnJson);
+        
+        ObjectMapper mapper = new ObjectMapper();
+        
+        DincdVO2 dincdVO2 = new DincdVO2();
+        dincdVO2 = mapper.readValue(ReturnJson, DincdVO2.class);
+        System.out.println("ResultCode:"+dincdVO2.getBusinessId());
+        for(int i=0; i<dincdVO2.getBasic_data().size(); i++){
+            Item_basic item = dincdVO2.getBasic_data().get(i);
+            System.out.println(item.toString());
         }
         
         try{
-            Gson gson = new Gson();
+            /*Gson gson = new Gson();
             DincdJsonVO imgVO = gson.fromJson(obj.toString(), DincdJsonVO.class);
             
-            if(imgVO.getBusinessId()!=0){
+            if(imgVO.getBusinessId()!=0){*/
 
-            //dincdService.deleteTblDincdInfo();
+           
          
-            List<Map<String,Object>> additional_data = imgVO.getAdditionnal_data();
-            List<Map<String,Object>> basic_data = imgVO.getBasic_data();
+            List<Item_additional> additional_data = dincdVO2.getAdditionnal_data();
+            List<Item_basic> basic_data = dincdVO2.getBasic_data();
                     
             Map<String,Object> input = new HashMap<String,Object>();
                     
-            input.put("business_id",imgVO.getBusinessId());
+            input.put("business_id",dincdVO2.getBusinessId());
                     
                     
             input.put("name",basic_data.get(0).get("name"));
